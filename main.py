f28d9c6eaac4042f4068e3442473860fd54dd5f6
rukawartjxodnd <40817166+rukawartjxodnd@users.noreply.github.com>
2021-01-20 15:31:44 +0900

    .

--- main.py
+++ main.py
@@ -3,6 +3,11 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
