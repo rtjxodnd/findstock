ed99bb6201493e1544e532a4f8cfe4f16e2a8c74
rukawartjxodnd <40817166+rukawartjxodnd@users.noreply.github.com>
2021-01-26 14:17:34 +0900

    .

--- main/execute.sh
+++ main/execute.sh
@@ -2,11 +2,11 @@
 echo '다중 파일 순차 실행해요'
 {
 echo '2019'
-/home/ubuntu/venv_findstock/bin/python3 /home/ubuntu/project_findstock/main/00_set_holiday_info.py '2019'
+/home/ubuntu/venv_findstock/bin/python3 /home/ubuntu/project_findstock/main/00_set_holiday_info.py '2019' > /home/ubuntu/project_findstock/logs/00_set_holiday_info.log
 echo '2020';
-/home/ubuntu/venv_findstock/bin/python3 /home/ubuntu/project_findstock/main/00_set_holiday_info.py '2020'
+/home/ubuntu/venv_findstock/bin/python3 /home/ubuntu/project_findstock/main/00_set_holiday_info.py '2020' > /home/ubuntu/project_findstock/logs/00_set_holiday_info.log
 echo '2021'
-/home/ubuntu/venv_findstock/bin/python3 /home/ubuntu/project_findstock/main/00_set_holiday_info.py '2021'
+/home/ubuntu/venv_findstock/bin/python3 /home/ubuntu/project_findstock/main/00_set_holiday_info.py '2021' > /home/ubuntu/project_findstock/logs/00_set_holiday_info.log
 echo '2022'
-/home/ubuntu/venv_findstock/bin/python3 /home/ubuntu/project_findstock/main/00_set_holiday_info.py '2022'
+/home/ubuntu/venv_findstock/bin/python3 /home/ubuntu/project_findstock/main/00_set_holiday_info.py '2022' > /home/ubuntu/project_findstock/logs/00_set_holiday_info.log
 }
